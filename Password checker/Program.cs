0973be1070986c2730041a4c37d4f6fa6b5cef35
Abdulkadir <qansax30@hotmail.com>
2024-11-09 00:10:55 +0100

    Add project files.

---
+++ Password checker/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            Console.Write("Enter password: ");
+            string passwordc   = Console.ReadLine();
+
+            Console.WriteLine(password);
+            Console.WriteLine(password);
+            if (!password.Equals(string.Empty) && !password.Equals(string.Empty))
+            {
+
+
+
+                //
+            }
+            else
+            {
+                Console.WriteLine("please enter a password .");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
